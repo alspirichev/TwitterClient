4c4a31723e20cd5d858bba95a8a1d49f1b6f9aa8
alspirichev <alspirichev@gmail.com>
2015-08-29 15:35:33 +0300

    Initial commit

---
+++ TwitterClient/ViewController.h
@@ -0,0 +1,26 @@
+//
+//  ViewController.h
+//  TwitterClient
+//
+//  Created by Alexander Spirichev on 29.08.15.
+//  Copyright (c) 2015 Alexander Spirichev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface ViewController : UIViewController <UITableViewDelegate, UITableViewDataSource, NSFetchedResultsControllerDelegate>
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (strong, nonatomic) NSMutableArray* twitterFeed;
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
